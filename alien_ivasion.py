f4cb8428e8555d5ef0afb93d36522a83b4ec55d3
Aigerim Raimbay <raimbay89@gmail.com>
2024-01-19 17:37:00 -0600

    refactored initial method

--- alien_ivasion.py
+++ alien_ivasion.py
@@ -22,18 +22,30 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+            
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        #Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #Move ship to the right.
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
